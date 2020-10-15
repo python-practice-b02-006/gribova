0d563e657e8d87da1d45cb0c5b3383fa25bc2720
gribovanika <gribova.ni@phystech.edu>
2020-10-15 22:14:35 +0500

    it is great

--- balls/my.py
+++ balls/my.py
@@ -4,37 +4,9 @@ Created on Fri Oct  9 11:40:35 2020
 
 @author: Nika
 """
-'''
-import pygame as pg
-import random
-
-FPS=10
-clock=pg.time.Clock()
-screen = pg.display.set_mode((640,480))
-done = False
-
-pg.font.init()
-font_1 = pg.font.SysFont('Times New Roman', 28)
-    
-
-#ball_coords=list([random.rand()]*4)
-class Balls():
-    def _init_(self, n_balls):
-        self.balls = list([[random.randn(0,639), random.randn(0,479)] for i in range(n_balls)])
-        
-        
-while not done:
-    clock.tick(10)
-    x, y = (0, 0)
-    for event in pg.event.get():
-        if event == pg.QUIT:
-            done = True
-        elif event == pg.MOUSEBUTTONDOWN:
-            x, y = event.pos
-    text_surf = font_1.render("{},{}".format(x,y),False,(255,0,0))'''
 import pygame
 from random import randint
-sc = 0
+
 
 LSALMON = (255, 160, 122)
 PEACH = (255, 218, 185)
@@ -54,9 +26,9 @@ class Balls():
         self.n = n_balls
         self.colors = list([COLORS[randint(0, len(COLORS)-1)] for i in range(n_balls)])
         self.radius = list([randint(10, 70) for i in range(n_balls)])
-        self.areas = list([[randint(0,1019-self.radius[i]), randint(0,639--self.radius[i])] for i in range(n_balls)])
-        self.speed = list([[randint(-5, 5), randint(-5,5)] for i in range(n_balls)])
-        
+        self.areas = list([[randint(0,1019-self.radius[i]), 
+                            randint(0,639-self.radius[i])] for i in range(n_balls)])
+        self.speed = list([[randint(-5, 5), randint(-5,5)] for i in range(n_balls)])      
         self.score = 0
     
     
@@ -69,17 +41,18 @@ class Balls():
     def tyk(self, x, y):
         global sc
         for i in range(self.n):
-            if (int(self.areas[i][0]) - int(x))**2 + (int(self.areas[i][1]) - int(y))**2 < self.radius[i]**2:                
+            if (self.areas[i][0] - x)**2 + (self.areas[i][1] - y)**2 < self.radius[i]**2:                
                 if self.colors[i] == GREY: 
                     self.score += 100                  
                 else:
                     self.score += (1000/self.radius[i])//3
                 sc = self.score
                 self.radius[i] = randint(10, 70)
-                self.areas[i] = [randint(0,1019-self.radius[i]), randint(0,639--self.radius[i])]
+                self.areas[i] = [randint(0,1019-self.radius[i]), 
+                                 randint(0,639-self.radius[i])]
                 self.speed[i] = [randint(-self.speed[i][0]-5, self.speed[i][1]+5), 
                                  randint(-self.speed[i][0]-5, self.speed[i][1]+5)]
-                return (True,sc)
+                return True
 
 
     def iterate(self, tick):
@@ -99,13 +72,15 @@ class Balls():
                 self.areas[i][1] = self.radius[i]
                 self.speed[i][1] *= -1    
 
+
 pygame.init()
 pygame.font.init()
 clock = pygame.time.Clock()
 
-FPS = 5
 screen = pygame.display.set_mode((1020, 640))
 
+FPS = 5
+sc = 0
 k = 0
 x = 0
 y = 0
@@ -139,25 +114,25 @@ while not finished:
     screen.blit(score_surf1, (10, 30))
     screen.blit(score_surf2, (100, 550))   
     pygame.display.update()
+
 finished = False
+
 while not finished:
     screen.fill(PEACH)
     clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            finished = True
-        
+            finished = True        
     if sc >= k*100:
         screen.blit(hooray_IMG,(50,100)) 
     else: 
         screen.blit(loser_IMG,(50,100)) 
-        score_surf3 = font_3.render("Ты что ещё не догадался на какой цвет нажимать?!? ", False, DARK)
+        score_surf3 = font_3.render("Ты что ещё не догадался на какой цвет нажимать?!?", False, DARK)
         screen.blit(score_surf3, (10, 30))
     score_surf4 = font_3.render("Максимальный результат {}, твой результат {}".format(100*k,sc), False, TOMATO)
     screen.blit(score_surf4, (10, 50))
 
     pygame.display.update()
     
-    #screen.blit(hooray_IMG,(200,200))    
 pygame.quit()
     
