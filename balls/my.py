f8c0abf4ebfc58f7ad223724ec71f7f1cb101690
gribovanika <gribova.ni@phystech.edu>
2020-10-22 23:28:02 +0500

    excelent

--- balls/my.py
+++ balls/my.py
@@ -6,6 +6,7 @@ Created on Fri Oct  9 11:40:35 2020
 """
 import pygame
 from random import randint
+import os.path
 
 
 LSALMON = (255, 160, 122)
@@ -30,30 +31,27 @@ class Balls():
                             randint(0,639-self.radius[i])] for i in range(n_balls)])
         self.speed = list([[randint(-5, 5), randint(-5,5)] for i in range(n_balls)])      
         self.score = 0
-    
-    
+       
     def draw_all_balls(self, screen):
         for i in range(self.n):
             pygame.draw.circle(screen, self.colors[i], 
                                self.areas[i], self.radius[i])
 
-
-    def tyk(self, x, y):
+    def click(self, x, y):
         global sc
         for i in range(self.n):
             if (self.areas[i][0] - x)**2 + (self.areas[i][1] - y)**2 < self.radius[i]**2:                
                 if self.colors[i] == GREY: 
                     self.score += 100                  
                 else:
-                    self.score += (1000/self.radius[i])//3
-                sc = self.score
+                    self.score += int((1000/self.radius[i])//3)
+                sc = int(self.score)
                 self.radius[i] = randint(10, 70)
                 self.areas[i] = [randint(0,1019-self.radius[i]), 
                                  randint(0,639-self.radius[i])]
                 self.speed[i] = [randint(-abs(self.speed[i][0])-5, abs(self.speed[i][1])+5), 
                                  randint(-abs(self.speed[i][0])-5, abs(self.speed[i][1])+5)]
                 return True
-
 
     def iterate(self, tick):
         for i, ball in enumerate(self.areas):
@@ -72,6 +70,17 @@ class Balls():
                 self.areas[i][1] = self.radius[i]
                 self.speed[i][1] *= -1    
 
+
+def sort_file(A):
+    k = 0
+    i = len(A)-1
+    while k == 0 and i > 0:
+        if int(A[i][4]) >= int(A[i - 1][4]):
+            A[i], A[i-1] = A[i-1], A[i]
+        else: k = 10
+        i -= 1
+    return A
+        
 
 pygame.init()
 pygame.font.init()
@@ -106,7 +115,7 @@ while not finished:
             y = event.pos[1]
             k += 1
     screen.fill((125,125,125))
-    balls.tyk(x, y)
+    balls.click(x, y)
     balls.iterate(1)
     balls.draw_all_balls(screen)
     score_surf1 = font_1.render("Score: {}".format(balls.score), False, (255, 0, 0))
@@ -116,6 +125,39 @@ while not finished:
     pygame.display.update()
 
 finished = False
+
+A=[]# Here I'm working with rating file.
+if os.path.exists('input.txt') == True:
+    input = open('input.txt', 'r')
+    A = input.readlines()
+    for i in range(len(A)):
+        A[i] = (A[i].split())
+    input.close()
+    input = open('input.txt','a')
+    print('{}-ый игрок смог набрать: {}'.format((len(A) + 1), int(sc)), file=input)
+    input.close()
+    input = open('input.txt','r')
+    A = input.readlines()
+    for i in range(len(A)):
+        A[i] = (A[i].split())
+    sort_file(A)
+    input.close()
+    input = open('input.txt', 'w') 
+    input.close()
+    for i in range(len(A)):
+        input = open('input.txt','a')
+        print(*A[i], file=input)
+        input.close()
+else:
+    input = open('input.txt','a')
+    print('{}-ый игрок смог набрать: {}'.format(1, int(sc)), file=input) 
+    input.close()
+
+rating=1
+for i in range(len(A)):
+    if int(A[i][4]) > sc:
+        rating += 1
+    else: break
 
 while not finished:
     screen.fill(PEACH)
@@ -129,10 +171,12 @@ while not finished:
         screen.blit(loser_IMG,(50,100)) 
         score_surf3 = font_3.render("Ты что ещё не догадался на какой цвет нажимать?!?", False, DARK)
         screen.blit(score_surf3, (10, 30))
-    score_surf4 = font_3.render("Максимальный результат {}, твой результат {}".format(100*k,sc), False, TOMATO)
+    score_surf4 = font_3.render("Максимальный результат больше {}, твой результат {}".format(100*k,sc), False, TOMATO)
     screen.blit(score_surf4, (10, 50))
+    score_surf5 = font_3.render("Твоя позиция в рейтинге {}".format(rating), False, SKYBLUE)
+    screen.blit(score_surf5, (10, 10))
 
     pygame.display.update()
-    
+  
 pygame.quit()
     
