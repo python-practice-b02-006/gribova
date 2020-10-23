b04da155d453eb06af91f7fd968c5e27657e9fa2
gribovanika <gribova.ni@phystech.edu>
2020-10-23 13:20:54 +0500

    gun goes up and down, and wathces your position

--- my_guns.py
+++ my_guns.py
@@ -10,6 +10,17 @@ from random import randint, gauss
 
 SIZE = (600, 600)
 BLACK = (0, 0, 0)
+LSALMON = (255, 160, 122)
+PEACH = (255, 218, 185)
+LEMONE = (255, 250, 205)
+SKYBLUE = (100, 149, 237)
+TOMATO = (255, 99, 71)
+CADET = (176, 196, 222)
+SLATE = (106, 90, 205)
+DARK = (72, 61, 109)
+BROWN = (139, 69, 19)
+GREY = (130, 130, 130)
+COLORS = [LSALMON, PEACH, LEMONE, SKYBLUE, TOMATO, GREY, GREY, GREY]
 
 pg.init()
 
@@ -22,15 +33,25 @@ class Ball():
 
 
 class Gun():
-    pass
-    '''def __init__(self, coord=[30, SIZE[1]//2]):
+    def __init__(self, coord=[30, SIZE[1]//2]):
         self.coord = coord
         self.angle = 0
     
     def draw(self,screen):
-        pass
+        end_pos = [self.coord[0] + 30*np.cos(self.angle),
+                   self.coord[1] + 30*np.sin(self.angle)]
+        pg.draw.lines(screen, SKYBLUE, False, [(self.coord[0], self.coord[1]), 
+                                               (self.coord[0], self.coord[1]+10),
+                                               (end_pos[0], end_pos[1]+10), 
+                                               (end_pos[0], end_pos[1]),
+                                               (self.coord[0], self.coord[1])], 2)
+        
+    def set_angle(self, mouse_pos):
+        self.angle = np.arctan2(mouse_pos[1] - self.coord[1], 
+                                mouse_pos[0] - self.coord[0])
+
     def strike(self):
-        pass'''
+        pass
 
 class Target():
     pass
@@ -48,13 +69,22 @@ class Manager():
         
     def draw(self, screen):
         screen.fill(BLACK)
-        # self.gun.draw(draw)
+        self.gun.draw(screen)
         
     def handle_events(self, events):
         done = False
         for event in events:
             if event.type == pg.QUIT:
                 done = True
+            elif event.type == pg.KEYDOWN:
+                if event.key == pg.K_UP:
+                    self.gun.coord[1] -= 20
+                elif event.key == pg.K_DOWN:
+                    self.gun.coord[1] += 20
+                    
+        if pg.mouse.get_focused():
+            mouse_pos = pg.mouse.get_pos()
+            self.gun.set_angle(mouse_pos)
         return done
 
 
@@ -71,3 +101,4 @@ while not done:
     done = mgr.process(pg.event.get(), screen)
     pg.display.flip()
 pg.quit()
+
