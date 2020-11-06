828d7d6125913fa0e237da53f7da166a6f8820c7
gribovanika <gribova.ni@phystech.edu>
2020-11-06 12:20:54 +0500

    new master

--- branches/branch_test.py
+++ branches/branch_test.py
@@ -5,5 +5,7 @@ Created on Fri Nov  6 11:59:30 2020
 @author: Nika
 """
 
-Class featuretest():
-    print(5)
+class FeatureClass():
+    def __init__(self):
+        print("new class constructor!")
+        
